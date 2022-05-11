925be95be1a0377569f40fa31f9dbe8942af02b4
abbasasadollahi <abasasadollahi@gmail.com>
2022-05-11 10:56:39 +0430

    index & Contact page aded

--- DevCard/Controllers/HomeController.cs
+++ DevCard/Controllers/HomeController.cs
@@ -6,19 +6,15 @@ namespace DevCard.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
-        }
 
+        }
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
