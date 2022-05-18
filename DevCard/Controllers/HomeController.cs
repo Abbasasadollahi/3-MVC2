cd75695022fd2c674434cc8f1be60c143ba4a635
abbasasadollahi <abasasadollahi@gmail.com>
2022-05-18 09:37:24 +0430

    ProjectDetails

--- DevCard/Controllers/HomeController.cs
+++ DevCard/Controllers/HomeController.cs
@@ -1,12 +1,21 @@
-﻿using DevCard.Models;
+﻿using DevCard.Data;
+using DevCard.Models;
 using DevCard_MVC.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Diagnostics;
 
 namespace DevCard.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly List<Service> _services = new List<Service>
+        {
+            new Service(1, "نقره ای"),
+            new Service(2, "طلایی"),
+            new Service(3, "پلاتین"),
+            new Service(4, "الماس"),
+        };
         public HomeController()
         {
 
@@ -15,15 +24,41 @@ namespace DevCard.Controllers
         {
             return View();
         }
+
+        public IActionResult ProjectDetails(long id)
+        {
+            var project = ProjectStore.GetProjectBy(id);
+            return View(project);
+        }
+
+        [HttpGet]
         public IActionResult Contact()
         {
-            var contact = new Contact();
-            return View(contact);
+            var model = new Contact()
+            {
+                Services = new SelectList(_services, "Id", "Name")
+            };
+            return View(model);
         }
         [HttpPost]
-        public IActionResult Contact(Contact contact)
+        public IActionResult Contact(Contact model)
         {
-            return View();
+            model.Services = new SelectList(_services, "Id", "Name");
+            if (!ModelState.IsValid == false)
+            {
+                ViewBag.error = "اطلاعات وارد شده صحیح نیست. لطفا دوباره تلاش کنید";
+                return View(model);
+            }
+            else
+            {
+                ModelState.Clear();
+                model = new Contact
+                {
+                    Services = new SelectList(_services, "Id", "Name")
+                };
+                ViewBag.success = "پیغام شما با موفقیت ارسال شد. باتشکر";
+                return View(model);
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- DevCard/ViewComponents/ProjectsViewComponent.cs
+++ DevCard/ViewComponents/ProjectsViewComponent.cs
@@ -1,4 +1,4 @@
-﻿using DevCard_MVC.Models;
+﻿using DevCard.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DevCard.ViewComponents
@@ -7,13 +7,7 @@ namespace DevCard.ViewComponents
     {
         public IViewComponentResult Invoke()
         {
-            var projects = new List<Project>
-            {
-                new Project(1, "تاکسی", "درخواست آنلاین تاکسی برای سفر های درون شهری", "project-1.jpg", "Atriya"),
-                new Project(2, "زودفود", "درخواست آنلاین غذا برای سراسر کشور", "project-2.jpg","ZoodFood"),
-                new Project(3, "مدارس", "سیستم مدیریت یکپارچه مدارس", "project-3.jpg","MONOP"),
-                new Project(4, "فضاپیما", "برنامه مدیریت فضاپیما های ناسا","project-4.jpg", "NASA"),
-            };
+            var projects = ProjectStore.GetProjects();
             return View("_Projects", projects);
         }
     }
